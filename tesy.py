3c2355c4505c87f1c6f9701c2237fb743157065d
Eryn LaNou <totallyfake.no@gmail.com>
2026-02-24 12:53:43 -0600

    Testing a thing

--- tesy.py
+++ tesy.py
@@ -3,13 +3,3 @@ import string
 import sys
 import array
 import random
-width = 7
-
-def discuss_whatever2():
-    writing = "this this"
-    print(textwrap.fill(writing,width))
-
-#def wrap_text(writing, width):
- #   return textwrap.fill(writing, width)
-
-discuss_whatever2()
